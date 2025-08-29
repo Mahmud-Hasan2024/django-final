48d92fd180f623e34ada3c08cfafcf52b504ea83
Mahmud Hasan <mh.kid234@gmail.com>
2025-08-29 12:59:36 +0600

    added default value for frontend url djoser

--- byte_feast/settings.py
+++ byte_feast/settings.py
@@ -193,8 +193,8 @@ SIMPLE_JWT = {
 }
 
 DJOSER = {
-    'EMAIL_FRONTEND_PROTOCOL': config('FRONTEND_PROTOCOL'),
-    'EMAIL_FRONTEND_DOMAIN': config('FRONTEND_DOMAIN'),
+    'EMAIL_FRONTEND_PROTOCOL': config('FRONTEND_PROTOCOL', default='http'),
+    'EMAIL_FRONTEND_DOMAIN': config('FRONTEND_DOMAIN', default='localhost:5173'),
     'EMAIL_FRONTEND_SITE_NAME': 'ByteFeast',
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': 'activate/{uid}/{token}',
