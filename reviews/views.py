01bc984576f31567e7836a0450016498913fbfa0
Mahmud Hasan <mh.kid234@gmail.com>
2025-09-03 03:42:23 +0600

    added error alert for preventing second review in reviws/views.py

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,11 @@
+from django.db import IntegrityError
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
-from rest_framework import serializers
 
 # Create your views here.
-
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = [IsAuthenticated]
@@ -17,9 +17,14 @@ class ReviewViewSet(ModelViewSet):
             return qs.filter(food_id=food_pk)
         return qs
 
-    
     def perform_create(self, serializer):
         food_id = self.kwargs.get('food_pk')
         if not food_id:
-            raise serializers.ValidationError("Food ID is required.")
-        serializer.save(user=self.request.user, food_id=food_id)
+            raise ValidationError("Food ID is required.")
+
+        try:
+            serializer.save(user=self.request.user, food_id=food_id)
+        except IntegrityError:
+            raise ValidationError(
+                {"detail": "You have already reviewed this product. Please update your review instead."}
+            )
