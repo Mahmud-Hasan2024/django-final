b49338013260670ef259d6050a567249452ce666
Mahmud Hasan <mh.kid234@gmail.com>
2025-09-03 11:03:16 +0600

    corrected cors allowed origin

--- byte_feast/settings.py
+++ byte_feast/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'byte_feast.wsgi.app'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
     "https://byte-feast-react-w8qi.vercel.app",
 ]
 
