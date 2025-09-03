c51b5d282b8c280c2c0c2ffdfcb6b2b78670c340
Mahmud Hasan <mh.kid234@gmail.com>
2025-09-03 11:27:59 +0600

    added cors header

--- byte_feast/settings.py
+++ byte_feast/settings.py
@@ -95,6 +95,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://byte-feast-react-w8qi.vercel.app",
+    "https://byte-feast-react.vercel.app",
 ]
 
 
